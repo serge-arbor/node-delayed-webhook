40ae51224126fb91d193396cd42c8bdb7b88b0d9
arbora <fuckall151055>
2021-10-30 23:34:40 +0300

    Fix - swagger tests conflict

--- src/http-api/swagger.ts
+++ src/http-api/swagger.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import swaggerUi from "swagger-ui-express";
-import * as swaggerDocument from "./../../public/swagger.json";
-
 const swaggerRouter = Router();
 
-swaggerRouter.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+try{
+    const swaggerDocument = require("./../../public/swagger.json");
+    swaggerRouter.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}catch(err){
+    console.log('Unable to load swagger.json', err);
+}
 
 export default swaggerRouter;
